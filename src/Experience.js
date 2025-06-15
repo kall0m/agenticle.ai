0778f8998016b1ec652a0fe4201a8e1637b1d532
Kaloyan Madzhunov <k.madjunov@gmail.com>
2025-06-15 18:19:15 +0300

    test rotation with useFrame

--- src/Experience.js
+++ src/Experience.js
@@ -1,8 +1,17 @@
+import { useFrame } from "@react-three/fiber";
+import { useRef } from "react";
+
 export default function Experience() {
+    const meshRef = useRef();
+
+    useFrame((state, delta) => {
+        meshRef.current.rotation.y += delta;
+    });
+
     return (
         <>
-            <mesh>
-                <sphereGeometry />
+            <mesh ref={meshRef}>
+                <torusKnotGeometry />
                 <meshNormalMaterial />
             </mesh>
         </>
